0ba396bbcf1907b33d6c59026858803bca9563cc
Nlucky320 <luckyes144@gmail.com>
2024-03-16 20:42:46 +0600

    banner button updated

--- src/assets/components/Banner/Banner.jsx
+++ src/assets/components/Banner/Banner.jsx
@@ -9,8 +9,8 @@ const Banner = () => {
       <h1 className="mb-5  md:text-[36px] lg:text-[52px] font-bold text-[#FFFFFF]  lg:w-[897px] mx-auto">Discover an exceptional cooking class tailored for you!</h1>
       <p className="mb-10 md:text-[18px] lg:w-[933px] mx-auto text-white">Stepping into the elegant ambiance of a fine dining restaurant, you are greeted by soft lighting, plush seating, and the gentle hum of conversation. The flavors dance on your palate, leaving you longing for just one more taste.</p>
                         <div className='space-x-6'>
-                              <button className="btn bg-[#0BE58A] btn-primary text-[#150B2B] rounded-[50px] px-6" >Explore Now</button>
-                              <button className="btn btn-primary text-white rounded-[50px] px-6">Our Feedback</button>
+                              <button className="btn bg-[#0BE58A] btn-primary text-[#150B2B] rounded-[50px] px-6 hover:bg-black hover:text-white border-none" >Explore Now</button>
+                              <button className="btn btn-primary text-white rounded-[50px] px-6 ">Our Feedback</button>
     </div>
     </div>
   </div>
